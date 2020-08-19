9828004ade5f3436d64457f3370da70c36673fbc
Malik DeJean <mdejean@MacBook-Pro.local>
2020-08-18 20:30:45 -0700

    completed all exercises

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
 import shoppingList from './shopping-list';
+import store from './store.js';
 
 const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
